27e5f4e76ce228d84d340f705fa42c8cd1d12c4b
Jeremy Dee <jeremydee@hotmail.com>
2013-03-01 10:11:42 -0600

    Tweaked gitignore

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+    $cTitle = "Empty Site";
     $cLine = "Welcome to ".$_SERVER['HTTP_HOST'].". The time is ".date('r');
-    echo $cLine;
+?>
+
+<html>
+<head><title><?php echo $cTitle?></title></head>
+<body>
+<?php echo $cLine; ?>
+</body>
+</html>
